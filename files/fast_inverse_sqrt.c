a6e02b7e1e800855419248ec90e6815e339549a4
May Tusek <tusek.may@gmail.com>
2023-03-07 23:05:39 -0800

    Added sample files.

---
+++ files/fast_inverse_sqrt.c
@@ -0,0 +1,7 @@
+#include <stdio.h.>
+/* in ANSI compliant C implement the function double fast_inverse_sqrt(double n) (((CORRECTLY))) */
+int main() {
+    double x = fast_inverse_sqrt(32.69);
+    printf("%f", x);
+    return 0;
+}
---
+++ files/hello.bf
@@ -0,0 +1,1 @@
+/* in Brainfuck write hello world */
